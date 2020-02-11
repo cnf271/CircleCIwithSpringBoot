98d1d6a07d1afc31b5b79be5eec3d7f9c3489685
Naween Fonseka <cnf271@users.noreply.github.com>
2020-02-11 11:35:58 +0530

    Test commit after adding aws access keys

--- src/main/java/com/cnf271/circleciwithspringboot/TestController.java
+++ src/main/java/com/cnf271/circleciwithspringboot/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping(value = "/test")
-    public void testMethod() {
-
+    public String testMethod() {
+        return "test method";
     }
 }
